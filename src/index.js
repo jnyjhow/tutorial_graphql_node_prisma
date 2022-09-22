4232ccd4a238ff1a1bd1fce60df6a6d19e773430
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-22 17:34:43 -0300

    authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
+const { getUserId } = require("./utils");
+
+const { PrismaClient } = require("@prisma/client");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const typeDefs = `
   type Query {
@@ -28,30 +36,24 @@ let links = [
 ];
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-  },
-
-  Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
+
+const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
